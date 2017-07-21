d0948c8cf8e6552ea6b74f26bea72e9758e5a0ad
Carlos Cordero <carlos.cordero@odigeo.com>
2017-07-21 13:01:55 +0200

    finished inventory app

---
+++ app/src/main/java/com/carloscordero/inventoryapp/CatalogActivity.java
@@ -0,0 +1,88 @@
+package com.carloscordero.inventoryapp;
+
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.carloscordero.inventoryapp.data.ProductDbHelper;
+import com.carloscordero.inventoryapp.data.ProductContract.ProductEntry;
+
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int PRODUCT_LOADER = 1;
+    private ListView mProductsListView;
+    private ProductsCursorAdapter mCursorAdapter;
+    private View noProductTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mProductsListView = (ListView) findViewById(R.id.list_view);
+        mCursorAdapter = new ProductsCursorAdapter(this, null);
+        mProductsListView.setAdapter(mCursorAdapter);
+
+        mProductsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+
+                Uri currentPetUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+                intent.setData(currentPetUri);
+
+                startActivity(intent);
+            }
+        });
+
+        noProductTextView = (TextView) findViewById(R.id.no_product_text_view);
+
+        mProductsListView.setEmptyView(noProductTextView);
+
+        getSupportLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                this,                               // Parent activity context
+                ProductEntry.CONTENT_URI,           // Table to query
+                null,                               // Projection to return
+                null,                               // No selection clause
+                null,                               // No selection arguments
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mCursorAdapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+    }
+}
